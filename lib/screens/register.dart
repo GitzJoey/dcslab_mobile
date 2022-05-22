fd4ee8318a99af7bb9ea50d259d75f0cfc495283
GitzJoey <gitzjoey@live.com>
2022-05-22 23:45:14 +0800

    initial commit

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -7,128 +7,114 @@ class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('images/bg.jpg'), fit: BoxFit.fill)),
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: Center(
-                  child: SizedBox(
-                width: 35,
-                height: 35,
-                child: Image.asset('images/g_logo.png'),
-              )),
-            ),
-            const Text('DCSLab',
-                style: TextStyle(
-                    fontSize: 45,
-                    fontWeight: FontWeight.w500,
-                    decoration: TextDecoration.none,
-                    color: Colors.black)),
-            const SizedBox(
-              height: 75,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 35),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Name',
-                  hintText: 'GitzJoey',
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 35),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                  hintText: 'gitzjoey@dcslab.com',
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 35),
-              child: TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
+        body: Container(
+            constraints: const BoxConstraints.expand(),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('images/bg.jpg'), fit: BoxFit.cover)),
+            child: Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                child: SingleChildScrollView(
+                    child: Column(children: [
+                  const SizedBox(height: 45),
+                  SizedBox(
+                    width: 35,
+                    height: 35,
+                    child: Image.asset('images/g_logo.png'),
+                  ),
+                  const Text('DCSLab',
+                      style: TextStyle(
+                          fontSize: 45,
+                          fontWeight: FontWeight.w500,
+                          decoration: TextDecoration.none,
+                          color: Colors.black)),
+                  const SizedBox(
+                    height: 75,
+                  ),
+                  const TextField(
+                      decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: '******************'),
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 35),
-              child: TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Confirm Password',
-                    hintText: '******************'),
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Container(
-              height: 45,
-              width: 200,
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              child: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'Sign Up',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-              ),
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const Login()));
-                },
-                child: const Text(
-                  'Back',
-                  style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Colors.blue,
-                      fontSize: 15),
-                )),
-            const SizedBox(
-              height: 100,
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                'Forgot Password',
-                style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.blue,
-                    fontSize: 15),
-              ),
-            )
-          ],
-        ),
-      ),
-    ));
+                    labelText: 'Name',
+                    hintText: 'GitzJoey',
+                  )),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      hintText: 'gitzjoey@dcslab.com',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const TextField(
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: '******************'),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const TextField(
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Confirm Password',
+                        hintText: '******************'),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                    height: 45,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Login()));
+                      },
+                      child: const Text(
+                        'Back',
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: Colors.blue,
+                            fontSize: 15),
+                      )),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.blue,
+                          fontSize: 15),
+                    ),
+                  )
+                ])))));
   }
 }
